2485b53f01f1733bf5658032ef19f871a161e88c
Michael Stapelberg <michael@stapelberg.de>
2021-01-08 09:16:29 +0100

    avrpower: parse json

--- avrpower.go
+++ avrpower.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"time"
 )
 
@@ -14,17 +14,33 @@ type avrPowerLoop struct {
 }
 
 func (l *avrPowerLoop) ProcessEvent(ev MQTTEvent) []MQTTPublish {
+	var lease struct {
+		Expiration time.Time `json:"expiration"`
+	}
 	switch ev.Topic {
 	case "runstatus/midna/i3lock":
-		l.midnaUnlocked = fmt.Sprintf("%s", ev.Payload) == `{"running":false}`
+		var runStatus struct {
+			Running bool `json:"running"`
+		}
+		if err := json.Unmarshal(ev.Payload.([]byte), &runStatus); err != nil {
+			l.statusf("unmarshaling runstatus: %v", err)
+			return nil
+		}
+		l.midnaUnlocked = !runStatus.Running
 
 	case "router7/dhcp4d/lease/Michaels-iPhone":
-		// TODO: json-unmarshal this out of the payload
-		l.michaelPhoneExpiration = time.Now().Add(20 * time.Minute)
+		if err := json.Unmarshal(ev.Payload.([]byte), &lease); err != nil {
+			l.statusf("unmarshaling router7 lease: %v", err)
+			return nil
+		}
+		l.michaelPhoneExpiration = lease.Expiration
 
 	case "router7/dhcp4d/lease/Galaxy-S10e":
-		// TODO: json-unmarshal this out of the payload
-		l.leaPhoneExpiration = time.Now().Add(20 * time.Minute)
+		if err := json.Unmarshal(ev.Payload.([]byte), &lease); err != nil {
+			l.statusf("unmarshaling router7 lease: %v", err)
+			return nil
+		}
+		l.leaPhoneExpiration = lease.Expiration
 
 	default:
 		return nil // event did not influence our state
@@ -33,8 +49,8 @@ func (l *avrPowerLoop) ProcessEvent(ev MQTTEvent) []MQTTPublish {
 	now := ev.Timestamp
 	weekday, hour, minute := now.Weekday(), now.Hour(), now.Minute()
 	_, _, _ = weekday, hour, minute // TODO
-	phoneHome := l.michaelPhoneExpiration.After(time.Now()) ||
-		l.leaPhoneExpiration.After(time.Now())
+	phoneHome := l.michaelPhoneExpiration.After(now) ||
+		l.leaPhoneExpiration.After(now)
 	l.statusf("midnaUnlocked=%v || (hour=%v > 8 && phoneHome=%v)",
 		l.midnaUnlocked,
 		hour,
