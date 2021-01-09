12be81610e15484786dff9c49f26adb5dd16888d
Michael Stapelberg <michael@stapelberg.de>
2021-01-09 10:51:14 +0100

    add test for avrPowerLoop

---
+++ avrpower_test.go
@@ -0,0 +1,152 @@
+package main
+
+import (
+	"encoding/json"
+	"testing"
+	"time"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestAvrPowerLoop(t *testing.T) {
+
+	t.Run("Unrelated", func(t *testing.T) {
+		l := &avrPowerLoop{}
+		publish := l.ProcessEvent(MQTTEvent{
+			Topic:     "unrelated/foo",
+			Timestamp: time.Now(),
+			Payload:   []byte("irrelevant"),
+		})
+		if len(publish) > 0 {
+			t.Errorf("event on unrelated topic unexpectedly resulted in a publish request")
+		}
+	})
+
+	t.Run("UnlockScreen", func(t *testing.T) {
+		l := &avrPowerLoop{}
+		got := l.ProcessEvent(MQTTEvent{
+			Topic:     "runstatus/midna/i3lock",
+			Timestamp: time.Now(),
+			Payload:   []byte(`{"running":false}`),
+		})
+		want := []MQTTPublish{
+			{
+				Topic:    "cmnd/tasmota_68462F/Power",
+				Payload:  "ON",
+				Retained: true,
+			},
+		}
+		if diff := cmp.Diff(want, got); diff != "" {
+			t.Errorf("unexpected MQTT publication: diff (-want +got):\n%s", diff)
+		}
+	})
+
+	t.Run("LockScreen", func(t *testing.T) {
+		l := &avrPowerLoop{}
+		got := l.ProcessEvent(MQTTEvent{
+			Topic:     "runstatus/midna/i3lock",
+			Timestamp: time.Now(),
+			Payload:   []byte(`{"running":true}`),
+		})
+		want := []MQTTPublish{
+			{
+				Topic:    "cmnd/tasmota_68462F/Power",
+				Payload:  "OFF",
+				Retained: true,
+			},
+		}
+		if diff := cmp.Diff(want, got); diff != "" {
+			t.Errorf("unexpected MQTT publication: diff (-want +got):\n%s", diff)
+		}
+	})
+
+	t.Run("PhoneHome", func(t *testing.T) {
+		l := &avrPowerLoop{}
+		timestamp := time.Date(
+			2021, time.January, 2,
+			// 09:23:21 UTC is after 08:00:00 in both, summer time (CEST) and
+			// winter time (CET) in Zürich.
+			9 /* hour */, 23 /* min */, 21 /* sec */, 0, /* nsec */
+			time.UTC)
+		lease := struct {
+			Expiration time.Time `json:"expiration"`
+		}{
+			Expiration: timestamp.Add(20 * time.Second),
+		}
+		payload, err := json.Marshal(&lease)
+		if err != nil {
+			t.Fatal(err)
+		}
+		got := l.ProcessEvent(MQTTEvent{
+			Topic:     "router7/dhcp4d/lease/Michaels-iPhone",
+			Timestamp: timestamp,
+			Payload:   payload,
+		})
+		want := []MQTTPublish{
+			{
+				Topic:    "cmnd/tasmota_68462F/Power",
+				Payload:  "ON",
+				Retained: true,
+			},
+		}
+		if diff := cmp.Diff(want, got); diff != "" {
+			t.Errorf("unexpected MQTT publication: diff (-want +got):\n%s", diff)
+		}
+	})
+
+	t.Run("PhoneHome", func(t *testing.T) {
+		l := &avrPowerLoop{}
+
+		// too early:
+		timestamp := time.Date(
+			2021, time.January, 2,
+			// 06:23:21 UTC is before 08:00:00 in both, summer time (CEST) and
+			// winter time (CET) in Zürich.
+			6 /* hour */, 23 /* min */, 21 /* sec */, 0, /* nsec */
+			time.UTC)
+		lease := struct {
+			Expiration time.Time `json:"expiration"`
+		}{
+			Expiration: timestamp.Add(6 * time.Hour),
+		}
+		payload, err := json.Marshal(&lease)
+		if err != nil {
+			t.Fatal(err)
+		}
+		got := l.ProcessEvent(MQTTEvent{
+			Topic:     "router7/dhcp4d/lease/Michaels-iPhone",
+			Timestamp: timestamp,
+			Payload:   payload,
+		})
+		want := []MQTTPublish{
+			{
+				Topic:    "cmnd/tasmota_68462F/Power",
+				Payload:  "OFF",
+				Retained: true,
+			},
+		}
+		if diff := cmp.Diff(want, got); diff != "" {
+			t.Errorf("unexpected MQTT publication: diff (-want +got):\n%s", diff)
+		}
+
+		timestamp = timestamp.Add(5 * time.Hour)
+		// only time passes, no other state change:
+		got = l.ProcessEvent(MQTTEvent{
+			Topic:     "regelwerk/ticker/1s",
+			Timestamp: timestamp,
+			Payload:   nil,
+		})
+
+		want = []MQTTPublish{
+			{
+				Topic:    "cmnd/tasmota_68462F/Power",
+				Payload:  "ON",
+				Retained: true,
+			},
+		}
+		if diff := cmp.Diff(want, got); diff != "" {
+			t.Errorf("unexpected MQTT publication: diff (-want +got):\n%s", diff)
+		}
+	})
+
+}
--- go.mod
+++ go.mod
@@ -5,5 +5,6 @@ go 1.16
 require (
 	github.com/antihax/optional v1.0.0
 	github.com/eclipse/paho.mqtt.golang v1.3.0
+	github.com/google/go-cmp v0.5.1
 	golang.org/x/oauth2 v0.0.0-20201208152858-08078c50e5b5
 )
