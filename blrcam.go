8b5d624795afd7338eeff88c49ca16ee30a10183
Michael Stapelberg <michael@stapelberg.de>
2026-01-24 08:54:10 +0100

    blrcam: add generic DHCP last ack metric

--- blrcam.go
+++ blrcam.go
@@ -18,6 +18,13 @@ var (
 			Help: "TODO",
 		})
 
+	blrLastDHCPAck = prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "blr_last_dhcp_ack",
+			Help: "TODO",
+		},
+		[]string{"hostname"})
+
 	blrCompLastAck = prometheus.NewGauge(
 		prometheus.GaugeOpts{
 			Name: "blr_comp_last_ack",
@@ -26,6 +33,7 @@ var (
 )
 
 func init() {
+	prometheus.MustRegister(blrLastDHCPAck)
 	prometheus.MustRegister(blrCamLastAck)
 	prometheus.MustRegister(blrCompLastAck)
 	// Initialize the last ack to the current time, not to zero.
@@ -33,6 +41,14 @@ func init() {
 	// on the scrape timeline, whereas a spurious last ack timestamp
 	// on restart is the lesser evil without consequences, other
 	// than maybe a jump in the dashboard.
+	for _, host := range []string{
+		"C210_4B4B07",
+		"C210_EED851",
+		"C210_EEE3E5",
+		"verkaufg9",
+	} {
+		blrLastDHCPAck.With(prometheus.Labels{"hostname": host}).Set(float64(time.Now().Unix()))
+	}
 	blrCamLastAck.Set(float64(time.Now().Unix()))
 	blrCompLastAck.Set(float64(time.Now().Unix()))
 }
@@ -79,19 +95,31 @@ func (l *blrCamLoop) ProcessEvent(ev MQTTEvent) []MQTTPublish {
 		return nil // do not scrape yet
 	}
 
-	lastAckCam, err := l.lastAckForLease("C210_4B4B07")
-	if err != nil {
-		l.statusf("blr/dhcp4d scrape error: %v", err)
-		return nil
+	for _, host := range []string{
+		"C210_4B4B07",
+		"C210_EED851",
+		"C210_EEE3E5",
+	} {
+		lastAckCam, err := l.lastAckForLease(host)
+		if err != nil {
+			l.statusf("blr/dhcp4d scrape error: %v", err)
+			return nil
+		}
+		blrLastDHCPAck.With(prometheus.Labels{"hostname": host}).Set(float64(lastAckCam.Unix()))
+		// TODO: remove this metric in favor of the generic one
+		if host == "C210_4B4B07" {
+			blrCamLastAck.Set(float64(lastAckCam.Unix()))
+		}
+		l.lastScrape = now
 	}
-	blrCamLastAck.Set(float64(lastAckCam.Unix()))
-	l.lastScrape = now
 
 	lastAckComp, err := l.lastAckForLease("verkaufg9")
 	if err != nil {
 		l.statusf("blr/dhcp4d scrape error: %v", err)
 		return nil
 	}
+	blrLastDHCPAck.With(prometheus.Labels{"hostname": "verkaufg9"}).Set(float64(lastAckComp.Unix()))
+	// TODO: remove this metric in favor of the generic one
 	blrCompLastAck.Set(float64(lastAckComp.Unix()))
 	l.lastScrape = now
 
